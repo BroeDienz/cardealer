c1795e13081c834d981c82dec472420ff5b9cae5
Broedienz <broe.dienz@gmail.com>
2025-06-08 17:03:59 +0700

    add input on dummycar.py

--- blog/management/commands/dummycar.py
+++ blog/management/commands/dummycar.py
@@ -2,14 +2,20 @@ import csv
 from decimal import Decimal
 from django.core.management.base import BaseCommand
 from blog.models import Car
+import os
 
 class Command(BaseCommand):
     help = 'Load dummy car data from a CSV file'
 
     def handle(self, *args, **kwargs):
-        # Path to the CSV file
-        csv_file_path = '/Users/mdienz/Documents/learn_project/cardealer/Dealer.csv'
-
+        # Input the CSV file
+        csv_file_path = input("Enter the path to the CSV file: ")
+        
+        # Check if the file exists
+        if not os.path.isfile(csv_file_path):
+            self.stdout.write(self.style.ERROR(f"File {csv_file_path} does not exist."))
+            return
+        
         # Open the CSV file and read its content
         with open(csv_file_path, 'r' , newline="") as csvfile:
             reader = csv.DictReader(csvfile, delimiter=';')
